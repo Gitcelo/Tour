a966aebd6d39b46b76c345b7cc9d2d2a4070dad2
Gitcelo <72700939+Gitcelo@users.noreply.github.com>
2021-04-13 10:19:43 +0000

    Lagaði gitignore

--- src/Model/TourDate.java
+++ src/Model/TourDate.java
@@ -1,8 +1,6 @@
 package Model;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class TourDate {
     private LocalDateTime date;
